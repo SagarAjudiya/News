c462b8fca875ce11766f8e242d99c10b8bf34fa0
Sagar Ajudiya <sagarajudiya2002@gmail.com>
2023-07-20 14:45:42 +0530

    change button UI

--- app/src/main/java/com/simform/news/ui/home/view/HomeActivity.kt
+++ app/src/main/java/com/simform/news/ui/home/view/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.simform.news.ui.home.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
@@ -20,6 +21,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
 
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         initViews()
     }
 
